29b005edf50cd1e72dc51e147bd9f0cb2b86793a
Your Name <youremail@example.com>
2023-06-07 21:06:56 +0700

    pickle module

--- recognition.py
+++ recognition.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import face_recognition
+import pickle
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 
@@ -13,6 +14,27 @@ def encode_known_faces(
 ) -> None:
     names = []
     encodings = []
-    for filepath in Path("training").glob("*/*"):
+    for filepath in Path("training_data").glob("*/*"):
         name = filepath.parent.name
         image = face_recognition.load_image_file(filepath)
+
+        face_locations = face_recognition.face_locations(image, model=model)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
+
+    name_encodings = {"names": names, "encodings": encodings}
+    with encodings_location.open(mode="wb") as f:
+        pickle.dump(name_encodings, f)
+
+def recognize_faces(
+    image_location: str,
+    model: str = "hog",
+    encodings_location: Path = DEFAULT_ENCODINGS_PATH,
+) -> None:
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+
+    input_image = face_recognition.load_image_file(image_location)
