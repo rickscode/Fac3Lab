52aadf08047eb3d12651d91eadd7b97729da33e7
Your Name <youremail@example.com>
2023-06-09 00:27:28 +0700

    Detect faces from command line.

--- recognition.py
+++ recognition.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-
 import face_recognition
 import pickle
+from collections import Counter
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 
@@ -29,6 +29,21 @@ def encode_known_faces(
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
+# encode training photos into pkl file_format
+# encode_known_faces()
+
+def _recognize_face(unknown_encoding, loaded_encodings):
+    boolean_matches = face_recognition.compare_faces(
+        loaded_encodings["encodings"], unknown_encoding
+    )
+    votes = Counter(
+        name
+        for match, name in zip(boolean_matches, loaded_encodings["names"])
+        if match
+    )
+    if votes:
+        return votes.most_common(1)[0][0]
+
 def recognize_faces(
     image_location: str,
     model: str = "hog",
@@ -38,3 +53,21 @@ def recognize_faces(
         loaded_encodings = pickle.load(f)
 
     input_image = face_recognition.load_image_file(image_location)
+
+    input_face_locations = face_recognition.face_locations(
+        input_image, model=model
+    )
+
+    input_face_encodings = face_recognition.face_encodings(
+        input_image, input_face_locations
+    )
+
+    for bounding_box, unknown_encoding in zip(
+        input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name:
+            name = "Unknown"
+        print(name, bounding_box)
+
+recognize_faces("notbill.jpg")
